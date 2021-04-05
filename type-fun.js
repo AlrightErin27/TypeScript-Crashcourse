c4ba54b7d9196c428458e24837cb291ec52b812a
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-05 15:30:11 -0700

    fin notes with Weston

--- type-fun.js
+++ type-fun.js
@@ -1,39 +1,53 @@
-console.clear();
-var helloTs = "Hello typescript";
-// console.log(helloTs);
-var boolyBoi = true;
-var countyBoi = 10;
-boolyBoi = false;
-countyBoi++;
-countyBoi *= 10;
-// console.log(countyBoi + 1);
-//any type is like a swiss army knife
-var sissyBoi = "string";
-sissyBoi = 111;
-//type inference
-var stringy = "yo man hi"; //type is now inferred to be a string
-//union --more than one type in a var
-var united;
-united = 20;
-united = "this is a string";
-//arrays in ts --only hold one data type
-var numList = [1, 2, 4, 1, 4];
-var genericArr = ["hi", "mushroom"];
-var numString = ["banana", 12];
-var mulitArrNums = [
-    [1, 5, 3, 0],
-    [12, -24, 9],
-];
-var thisVar = 10;
-thisVar = "shrimp";
-//let us make a fxn
+// console.clear();
+// let helloTs: string = "Hello typescript";
+// // console.log(helloTs);
+// let boolyBoi: boolean = true;
+// let countyBoi: number = 10;
+// boolyBoi = false;
+// countyBoi++;
+// countyBoi *= 10;
+// // console.log(countyBoi + 1);
+// //any type is like a swiss army knife
+// let sissyBoi: any = "string";
+// sissyBoi = 111;
+// //type inference
+// let stringy = "yo man hi"; //type is now inferred to be a string
+// //union --more than one type in a var
+// let united: string | number;
+// united = 20;
+// united = "this is a string";
+// //arrays in ts --only hold one data type
+// let numList: number[] = [1, 2, 4, 1, 4];
+// let genericArr: Array<string> = ["hi", "mushroom"];
+// let numString: (string | number)[] = ["banana", 12];
+// let mulitArrNums: number[][] = [
+//   [1, 5, 3, 0],
+//   [12, -24, 9],
+// ];
+// //create your own type
+// type stringOrNum = string | number;
+// let thisVar: stringOrNum = 10;
+// thisVar = "shrimp";
+// //let us make a fxn
 function compliment(person) {
     return "I like your haircut " + person + ".";
 }
-// console.log(compliment("Henry"));
-function complimentMany(people) {
-    //in callback fxns it will you type inference
-    people.forEach(function (person) {
-        console.log("Hey " + person + ", nice shoes!");
-    });
-}
+console.log(compliment("Henry"));
+//to use an interface -- it is like a type
+var typeScriptLover = {
+    name: "Weston",
+    saying: function () { return console.log("siiiiiick"); }
+};
+typeScriptLover.saying();
+//tuples - an arr of fixed length
+//declare all types that are going to be in it
+var twople = ["pears", 10];
+// twople = [13, "this won't work"];
+//Enums
+var Color;
+(function (Color) {
+    Color[Color["Green"] = 0] = "Green";
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Yellow"] = 2] = "Yellow";
+})(Color || (Color = {}));
+var colorChoice = Color.Yellow;
--- type-fun.ts
+++ type-fun.ts
@@ -1,55 +1,86 @@
-console.clear();
+// console.clear();
 
-let helloTs: string = "Hello typescript";
+// let helloTs: string = "Hello typescript";
 
-// console.log(helloTs);
+// // console.log(helloTs);
 
-let boolyBoi: boolean = true;
-let countyBoi: number = 10;
+// let boolyBoi: boolean = true;
+// let countyBoi: number = 10;
 
-boolyBoi = false;
-countyBoi++;
-countyBoi *= 10;
+// boolyBoi = false;
+// countyBoi++;
+// countyBoi *= 10;
 
-// console.log(countyBoi + 1);
+// // console.log(countyBoi + 1);
 
-//any type is like a swiss army knife
-let sissyBoi: any = "string";
-sissyBoi = 111;
+// //any type is like a swiss army knife
+// let sissyBoi: any = "string";
+// sissyBoi = 111;
 
-//type inference
-let stringy = "yo man hi"; //type is now inferred to be a string
+// //type inference
+// let stringy = "yo man hi"; //type is now inferred to be a string
 
-//union --more than one type in a var
-let united: string | number;
-united = 20;
-united = "this is a string";
+// //union --more than one type in a var
+// let united: string | number;
+// united = 20;
+// united = "this is a string";
 
-//arrays in ts --only hold one data type
-let numList: number[] = [1, 2, 4, 1, 4];
-let genericArr: Array<string> = ["hi", "mushroom"];
-let numString: (string | number)[] = ["banana", 12];
+// //arrays in ts --only hold one data type
+// let numList: number[] = [1, 2, 4, 1, 4];
+// let genericArr: Array<string> = ["hi", "mushroom"];
+// let numString: (string | number)[] = ["banana", 12];
 
-let mulitArrNums: number[][] = [
-  [1, 5, 3, 0],
-  [12, -24, 9],
-];
+// let mulitArrNums: number[][] = [
+//   [1, 5, 3, 0],
+//   [12, -24, 9],
+// ];
 
-//create your own type
-type stringOrNum = string | number;
+// //create your own type
+// type stringOrNum = string | number;
 
-let thisVar: stringOrNum = 10;
-thisVar = "shrimp";
+// let thisVar: stringOrNum = 10;
+// thisVar = "shrimp";
 
-//let us make a fxn
+// //let us make a fxn
 function compliment(person: string): string {
   return `I like your haircut ${person}.`;
 }
-// console.log(compliment("Henry"));
+console.log(compliment("Henry"));
 
-function complimentMany(people: string[]): void {
-  //in callback fxns it will you type inference
-  people.forEach((person) => {
-    console.log(`Hey ${person}, nice shoes!`);
-  });
+// function complimentMany(people: string[]): void {
+//   //in callback fxns it will you type inference
+//   people.forEach((person) => {
+//     console.log(`Hey ${person}, nice shoes!`);
+//   });
+// }
+
+//Interfaces-- sort of like object factories
+
+interface Person {
+  //define key value pairs
+  //readonly means can't be changed
+  readonly name: string;
+  //the ? makes it an optional attribute
+  age?: number;
+  //functionName(): returnValue
+  saying(): void;
 }
+//to use an interface -- it is like a type
+const typeScriptLover: Person = {
+  name: "Weston",
+  saying: () => console.log("siiiiiick"),
+};
+typeScriptLover.saying();
+
+//tuples - an arr of fixed length
+//declare all types that are going to be in it
+let twople: [string, number] = ["pears", 10];
+// twople = [13, "this won't work"];
+
+//Enums
+enum Color {
+  Green,
+  Red,
+  Yellow,
+}
+let colorChoice: Color = Color.Yellow;
