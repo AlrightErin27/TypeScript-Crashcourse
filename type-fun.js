0431ac73dee8a713273ba52b73aa3b678af1e523
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-05 15:14:55 -0700

    added notes and examples

--- type-fun.js
+++ type-fun.js
@@ -1,2 +1,39 @@
-var helloTs = "hello typescript";
-console.log(helloTs);
+console.clear();
+var helloTs = "Hello typescript";
+// console.log(helloTs);
+var boolyBoi = true;
+var countyBoi = 10;
+boolyBoi = false;
+countyBoi++;
+countyBoi *= 10;
+// console.log(countyBoi + 1);
+//any type is like a swiss army knife
+var sissyBoi = "string";
+sissyBoi = 111;
+//type inference
+var stringy = "yo man hi"; //type is now inferred to be a string
+//union --more than one type in a var
+var united;
+united = 20;
+united = "this is a string";
+//arrays in ts --only hold one data type
+var numList = [1, 2, 4, 1, 4];
+var genericArr = ["hi", "mushroom"];
+var numString = ["banana", 12];
+var mulitArrNums = [
+    [1, 5, 3, 0],
+    [12, -24, 9],
+];
+var thisVar = 10;
+thisVar = "shrimp";
+//let us make a fxn
+function compliment(person) {
+    return "I like your haircut " + person + ".";
+}
+// console.log(compliment("Henry"));
+function complimentMany(people) {
+    //in callback fxns it will you type inference
+    people.forEach(function (person) {
+        console.log("Hey " + person + ", nice shoes!");
+    });
+}
--- type-fun.ts
+++ type-fun.ts
@@ -1,3 +1,55 @@
+console.clear();
+
 let helloTs: string = "Hello typescript";
 
-console.log(helloTs);
+// console.log(helloTs);
+
+let boolyBoi: boolean = true;
+let countyBoi: number = 10;
+
+boolyBoi = false;
+countyBoi++;
+countyBoi *= 10;
+
+// console.log(countyBoi + 1);
+
+//any type is like a swiss army knife
+let sissyBoi: any = "string";
+sissyBoi = 111;
+
+//type inference
+let stringy = "yo man hi"; //type is now inferred to be a string
+
+//union --more than one type in a var
+let united: string | number;
+united = 20;
+united = "this is a string";
+
+//arrays in ts --only hold one data type
+let numList: number[] = [1, 2, 4, 1, 4];
+let genericArr: Array<string> = ["hi", "mushroom"];
+let numString: (string | number)[] = ["banana", 12];
+
+let mulitArrNums: number[][] = [
+  [1, 5, 3, 0],
+  [12, -24, 9],
+];
+
+//create your own type
+type stringOrNum = string | number;
+
+let thisVar: stringOrNum = 10;
+thisVar = "shrimp";
+
+//let us make a fxn
+function compliment(person: string): string {
+  return `I like your haircut ${person}.`;
+}
+// console.log(compliment("Henry"));
+
+function complimentMany(people: string[]): void {
+  //in callback fxns it will you type inference
+  people.forEach((person) => {
+    console.log(`Hey ${person}, nice shoes!`);
+  });
+}
